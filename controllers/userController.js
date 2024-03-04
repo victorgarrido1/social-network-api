959a4e8e237ffd4f4bb28f66aad6ca8ad4a30d9e
victorgarrido1 <vcgarr94@gmail.com>
2024-03-03 22:12:26 -0700

    appling user controller routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,71 @@ const getUsers = {
             console.error("Error creating user:", err);
             return res.status(500).json({ error: "Internal server error" });
         }
-    }
+    },
+
+    updateUser: async (req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+    
+            if (!user) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+    
+            return res.status(200).json(user);
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    //Delete a user here
+    deleteUser: async (req, res) => {
+        try {
+            const user = await User.findByIdAndDelete(req.params.userId);
+    
+            if (!user) {
+                return res.status(404).json({ message: "No user with this ID" });
+            }
+    
+            // Also delete the thoughts associated with the user
+            await Thought.deleteMany({ username: user._id });
+    
+            return res.status(200).json({ message: "User deleted successfully" });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    //adding a friend here
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { friends: params.friendId } },
+          { new: true }
+        )
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+
+
+      //delete a friend 
+        deleteFriend({ params }, res {
+            User.findOneAndUpdate(
+                {_id: params.userId },
+                { $pull: }
+            )
+        })
 };
 
 module.exports = getUsers;
